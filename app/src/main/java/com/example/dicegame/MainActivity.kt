6d639cd9e5b82cddca35d678e769e37d01f348a1
abhi-sri07 <sri555abhi@gmail.com>
2024-07-05 19:06:23 +0530

    Buttons are laid out in a loop instead of being hard-coded.

--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -10,10 +10,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,8 +28,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,37 +40,28 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = {
-                            Text(text = stringResource(R.string.dice_game))
-                        },
-                        colors = TopAppBarDefaults.topAppBarColors(
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            titleContentColor = MaterialTheme.colorScheme.onPrimary
-                        )
-                    )
-                }
-            ) {
-                MainScreen(modifier = Modifier
-                    .padding(it)
-                    .fillMaxSize())
-            }
+            MainScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    val noOfRounds = 3  //should stay 3 till I find a better way
+
     var currentScore by remember {
+        mutableIntStateOf(0)
+    }
+
+    var highScore by rememberSaveable {
         mutableIntStateOf(0)
     }
 
@@ -70,23 +69,15 @@ fun MainScreen(modifier: Modifier = Modifier) {
         mutableIntStateOf(6)
     }
 
-    var roll1enabled by remember {
-        mutableStateOf(true)
+    val rollEnabled = remember {
+        mutableStateListOf(true, true, true)
     }
 
-    var roll2enabled by remember {
-        mutableStateOf(true)
-    }
-
-    var roll3enabled by remember {
-        mutableStateOf(true)
-    }
-
-    var rollsCompleted by remember {
+    var allRollsCompleted by remember {
         mutableStateOf(false)
     }
 
-    val scoreType = if (rollsCompleted) "Final" else "Current"
+    val scoreType = if (allRollsCompleted) "Final" else "Current"
 
     val currentImage = when (rollResult) {
         1 -> R.drawable.dice_1
@@ -96,62 +87,95 @@ fun MainScreen(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.dice_game))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {
+                    rollEnabled.fill(true)
+                    highScore = max(highScore, currentScore)
+                    currentScore = 0
+                    allRollsCompleted = false
+                },
+                icon = { Icon(Icons.Filled.Refresh, "Extended floating action button.") },
+                text = { Text(text = "New Game", fontSize = 20.sp) },
+                modifier = Modifier.padding(bottom = 36.dp)
+            )
+        },
+
+        floatingActionButtonPosition = FabPosition.Center
     ) {
-        Text(
-            text = "$scoreType Score: $currentScore",
-            fontSize = 24.sp,
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Image(
-            painter = painterResource(id = currentImage),
-            contentDescription = rollResult.toString()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        Column(
+            modifier = modifier
+                .padding(it)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(
-                onClick = {
-                    rollResult = (1..6).random()    //pick a random number
-                    currentScore += rollResult  //update current score
-                    roll1enabled = false    //to disable button after use
-                },
-                enabled = roll1enabled
-            ) {
-                Text(text = "Roll 1", fontSize = 20.sp)
-            }
 
-            Button(
-                onClick = {
-                    rollResult = (1..6).random()
-                    currentScore += rollResult
-                    roll2enabled = false
-                },
-                enabled = roll2enabled
-            ) {
-                Text(text = "Roll 2", fontSize = 20.sp)
-            }
+            Text(
+                text = "High Score: $highScore",
+                fontSize = 24.sp
+            )
 
-            Button(
-                onClick = {
-                    rollResult = (1..6).random()
-                    currentScore += rollResult
-                    roll3enabled = false
-                    rollsCompleted = true   //all rolls are over
-                },
-                enabled = roll3enabled
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = "$scoreType Score: $currentScore",
+                fontSize = 28.sp,
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = currentImage),
+                contentDescription = rollResult.toString()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Text(text = "Roll 3", fontSize = 20.sp)
+
+                for (i in (0..<noOfRounds)) {
+                    Button(
+                        onClick = {
+                            rollResult = (1..6).random()    //pick a random number
+                            currentScore += rollResult  //update current score
+                            rollEnabled[i] = false  //disable button after use
+
+                            //after last round
+                            if (i == noOfRounds - 1) {
+                                allRollsCompleted = true
+                                highScore = max(highScore, currentScore)
+                            }
+                        },
+                        enabled = rollEnabled[i]
+                    ) {
+                        Text(text = "Roll ${i + 1}", fontSize = 20.sp)
+                    }
+                }
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
 }
